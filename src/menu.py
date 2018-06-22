1b30437415128c6c66fd7c0a8293f4701c566b11
AugustoConti <augustoconti10@gmail.com>
2018-06-21 21:12:03 -0300

    Cambiado padding de item

--- src/menu.py
+++ src/menu.py
@@ -3,11 +3,15 @@ from consolemenu.format import MenuBorderStyleType
 from consolemenu.menu_component import Dimension
 
 
-menu_format = MenuFormatBuilder(max_dimension=Dimension(width=55, height=40)) \
+menu_format = MenuFormatBuilder(max_dimension=Dimension(width=50, height=40)) \
     .set_border_style_type(MenuBorderStyleType.HEAVY_BORDER) \
     .set_title_align('center') \
     .set_subtitle_align('center') \
-    .show_header_bottom_border(True)
+    .show_header_bottom_border(True) \
+    .set_items_left_padding(0)\
+    .set_items_top_padding(0)\
+    .set_items_bottom_padding(0)\
+    .set_items_right_padding(0)
 
 
 def selection_menu(strings, title):
