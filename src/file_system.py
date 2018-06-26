1ce0733cd4fa43e9ae6d9acf29e5f270269af85f
Augusto <augustoconti10@gmail.com>
2018-06-26 12:50:54 -0300

    Implementando file system

--- src/file_system.py
+++ src/file_system.py
@@ -1,27 +1,48 @@
 from termcolor import colored
 
 
-# TODO print(name), listar(archivos que contiene), execute, cd
+# TODO execute
 # TODO directorio actual
 
 
 class FileSystem:
     def __init__(self):
-        pass
+        self._raiz = Folder('/', [])
+        self._actual = self._raiz
+
+    def cd(self, folder):
+        if self._actual.contain(folder):
+            self._actual = self._actual.cd(folder)
+
+    def listar(self):
+        self._actual.listar()
+
+    def exe(self, prog):
+        if self._actual.contain(prog):
+            pass
 
 
 class File:
     def __init__(self, name):
-        self._name = name
+        self.name = name
 
     def __repr__(self):
-        return self._name
+        return self.name
 
 
 class Folder:
     def __init__(self, name, files):
-        self._name = name
+        self.name = name
         self._files = files
 
+    def contain(self, prog):
+        return len([p for p in self._files if p.name == prog])>0
+
+    def cd(self, folder):
+        return [f for f in self._files if f.name == folder][0]
+
+    def listar(self):
+        [print('\n', f) for f in self._files]
+
     def __repr__(self):
-        return colored(self._name, 'cyan')
+        return colored(self.name, 'cyan')
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ from src.hardware.asm import ASM
 from src.hardware.interruptions import Interruption
 from src.hardware.irq import IRQ
 
-__all__ = ['execute_programs', 'input_default']
+__all__ = ['execute_programs', 'execute_program', 'input_default']
 
 
 def input_default(msj, default):
@@ -10,21 +10,16 @@ def input_default(msj, default):
 
 
 def execute_programs(interrupt_vector):
-    p = Program(interrupt_vector)
-    p.execute("prg1.exe", 3)
-    p.execute("prg2.exe", 1)
-    p.execute("prg3.exe", 5)
-    p.execute("prg4.exe", 5)
-    p.execute("prg5.exe", 4)
-    p.execute("prg6.exe", 3)
+    execute_program(interrupt_vector, "prg1.exe", 3)
+    execute_program(interrupt_vector, "prg2.exe", 1)
+    execute_program(interrupt_vector, "prg3.exe", 5)
+    execute_program(interrupt_vector, "prg4.exe", 5)
+    execute_program(interrupt_vector, "prg5.exe", 4)
+    execute_program(interrupt_vector, "prg6.exe", 3)
 
 
-class Program:
-    def __init__(self, interrupt_vector):
-        self._interrupt_vector = interrupt_vector
-
-    def execute(self, program, priority=3):
-        self._interrupt_vector.handle(IRQ(Interruption.NEW, {'program': program, 'priority': priority}))
+def execute_program(interrupt_vector, program, priority=3):
+    interrupt_vector.handle(IRQ(Interruption.NEW, {'program': program, 'priority': priority}))
 
 
 def expand(instructions):
